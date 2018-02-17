c2104b5a2f1f1064dcd363c124946c2da5c8d6d1
Arunas Norvaisa <an@86.lt>
2018-02-17 10:53:09 +0200

    Pridėtas figuros.turis, tūris įtrauktas į lentelę

--- js/20180215-19/script/script.js
+++ js/20180215-19/script/script.js
@@ -1,21 +1,30 @@
-/* HTML puslapyje turi būti du mygtukai su pavadinimais: "Pradiniai duomenys" ir "Rasti max".
-
-    Paspaudus mygtuką "Pradiniai duomenys" atspausdinkite visas figūras lentelėje
-(naudokite <table>).
-
-    Paspaudus mygtuką "Rasti max" suskaičiuokite visų figūrų tūrius ir raskite figūrą kurios
-tūris didžiausias.
-    Atspausdinkite visas figūras lentelėje su suskaičiuotais tūriais, o figūrą, kurios tūris
-didžiausias, atspausdinkite kitokia spalva (spausdindami panaudokite css klasę, pvz:
-<td class=“raudona”>...</td>)*/
-
+/*
 var figuros = [
-    {ilgis: 2, plotis: 5, aukstis: 3},
-    {ilgis: 3, plotis: 2, aukstis: 6},
-    {ilgis: 1, plotis: 5, aukstis: 5}
+    { ilgis: 2, plotis: 5, aukstis: 3 },
+    { ilgis: 3, plotis: 2, aukstis: 6 },
+    { ilgis: 1, plotis: 5, aukstis: 5 }
 ];
 
-document.querySelector('#primary-data').addEventListener('click',function() {
+HTML puslapyje turi būti du mygtukai su pavadinimais: "Pradiniai duomenys" ir
+"Rasti max".
+
+Paspaudus mygtuką "Pradiniai duomenys" atspausdinkite visas figūras lentelėje
+(naudokite <table>).
+
+Paspaudus mygtuką "Rasti max" suskaičiuokite visų figūrų tūrius ir raskite
+figūrą kurios tūris didžiausias.
+
+Atspausdinkite visas figūras lentelėje su suskaičiuotais tūriais, o figūrą,
+kurios tūris didžiausias, atspausdinkite kitokia spalva (spausdindami panaudokite
+css klasę, pvz: <td class=“raudona”>...</td>)*/
+
+var figuros = [
+    { ilgis: 2, plotis: 5, aukstis: 3 },
+    { ilgis: 3, plotis: 2, aukstis: 6 },
+    { ilgis: 1, plotis: 5, aukstis: 5 }
+];
+
+document.querySelector('#primary-data').addEventListener('click', function() {
     printData(figuros);
 })
 
@@ -23,14 +32,16 @@ function printData(data) {
     var table = document.querySelector('.table');
 
     var tbody = document.querySelector('tbody');
-    if(tbody != null) {
+    if (tbody != null) {
         tbody.remove();
     }
 
     var tableBody = document.createElement('tbody');
 
-    for(var i = 0; i < data.length; i ++) {
+    for (var i = 0; i < data.length; i++) {
         var tableBodyRow = document.createElement('tr');
+        var turis = data[i].aukstis * data[i].plotis * data[i].ilgis;
+        data[i].turis = turis;
 
         var td = document.createElement("td")
         td.appendChild(document.createTextNode(data[i].ilgis));
@@ -46,8 +57,17 @@ function printData(data) {
         td.appendChild(document.createTextNode(data[i].aukstis));
         td.classList.add("text-center");
         tableBodyRow.appendChild(td);
+
+        var td = document.createElement("td")
+        td.appendChild(document.createTextNode(turis));
+        td.classList.add("text-center");
+        tableBodyRow.appendChild(td);
+        console.log(figuros);
+
         tableBody.appendChild(tableBodyRow);
     }
 
     table.appendChild(tableBody);
 }
+
+function rastiMax() {}
