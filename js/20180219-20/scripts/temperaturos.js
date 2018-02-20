93037ac4a6edce784743330a05d0b3a64c6a3e96
Arunas Norvaisa <an@86.lt>
2018-02-20 11:24:39 +0200

    Padaryta temperatūrų konvertacija

--- js/20180219-20/scripts/temperaturos.js
+++ js/20180219-20/scripts/temperaturos.js
@@ -1,6 +1,18 @@
-document.getElementById("C").addEventListener("oninput", cToFToK());
-function cToFToK() {
-    var x = document.getElementById("C").value;
-    document.getElementById("F").value = x * 1.8 + 32;
-    document.getElementById("K").value = x * 1.8 + 32;
+  function tKonvertuotojas(ivestis, reiksme) {
+  var tempC = document.getElementById("tC");
+  var tempF = document.getElementById("tF");
+  var tempK = document.getElementById("tK");
+  reiksme = parseFloat(reiksme); // be šito veikė, bet gana įdomiai :)
+    if (ivestis == "tC") {
+    tempF.value = ((reiksme * 1.8) + 32).toFixed(2);
+    tempK.value = (reiksme + 273.15).toFixed(2);
+  }
+  if (ivestis == "tF") {
+    tempC.value = ((reiksme - 32) / 1.8).toFixed(2);
+    tempK.value = (((reiksme - 32) / 1.8) + 273.15).toFixed(2);
+  }
+  if (ivestis == "tK") {
+    tempC.value = (reiksme - 273.15).toFixed(2);
+    tempF.value = (((reiksme - 273.15) * 1.8) + 32).toFixed(2);
+  }
 }
