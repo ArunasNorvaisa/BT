0c0cb659205a84b0439591fa609045e362bc4ff7
Arunas Norvaisa <an@86.lt>
2018-02-22 10:24:15 +0200

    Padarytas projektas su modaliniu langu

--- js/20180220-21/scripts/modalinis.js
+++ js/20180220-21/scripts/modalinis.js
@@ -1,60 +1,48 @@
-var modal = document.getElementById('myModal');
+var modal = document.getElementById('langas');
 var btn = document.getElementById("pridetiEilute");
 var span = document.getElementsByClassName("close")[0];
-var istrinti = document.getElementsByTagName("forma");
+var istrinti = document.getElementById("forma");
 var ivesti = document.getElementById("ivestiDuomenis");
 
 btn.onclick = function() {
-    modal.style.display = "block";
+  modal.style.display = "block";
 }
 
 span.onclick = function() {
-    modal.style.display = "none";
-}
-/*
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-*/
-istrinti.onclick = function () {
-    istrinti.value = '';
+  modal.style.display = "none";
 }
 
-// ivesti.onclick = function eilute() {
+istrinti.onclick = function() {
+  istrinti.value = '';
+}
+
 function eilute() {
-    var table = document.querySelector('.table');
-    var tbody = document.querySelector('tbody');
-    // var tableBody = document.createElement('tbody');
-    var tableBody = document.querySelector('tbody');
-    var tableBodyRow = document.createElement('tr');
-    var vardas = document.getElementById("klientoVardas").value;
-    var pavarde = document.getElementById("klientoPavarde").value;
-    var pastas = document.getElementById("klientoPastas").value;
-    var tel = document.getElementById("klientoTelefonas").value;
+  var table = document.querySelector('.table');
+  var tbody = document.querySelector('tbody');
+  var tableBody = document.querySelector('tbody');
+  var tableBodyRow = document.createElement('tr');
+  var vardas = document.getElementById("klientoVardas").value;
+  var pavarde = document.getElementById("klientoPavarde").value;
+  var pastas = document.getElementById("klientoPastas").value;
+  var tel = document.getElementById("klientoTelefonas").value;
 
-        var td = document.createElement("td")
-        td.appendChild(document.createTextNode(vardas));
-        td.classList.add("text-center");
-        tableBodyRow.appendChild(td);
+  var td = document.createElement("td")
+  td.appendChild(document.createTextNode(vardas));
+  tableBodyRow.appendChild(td);
 
-        var td = document.createElement("td")
-        td.appendChild(document.createTextNode(pavarde));
-        td.classList.add("text-center");
-        tableBodyRow.appendChild(td);
+  var td = document.createElement("td")
+  td.appendChild(document.createTextNode(pavarde));
+  tableBodyRow.appendChild(td);
 
-        var td = document.createElement("td")
-        td.appendChild(document.createTextNode(pastas));
-        td.classList.add("text-center");
-        tableBodyRow.appendChild(td);
+  var td = document.createElement("td")
+  td.appendChild(document.createTextNode(pastas));
+  tableBodyRow.appendChild(td);
 
-        var td = document.createElement("td")
-        td.appendChild(document.createTextNode(tel));
-        td.classList.add("text-center");
-        tableBodyRow.appendChild(td);
+  var td = document.createElement("td")
+  td.appendChild(document.createTextNode(tel));
+  tableBodyRow.appendChild(td);
 
-        tableBody.appendChild(tableBodyRow);
-        modal.style.display = "none";
-        istrinti.reset();
+  tableBody.appendChild(tableBodyRow);
+  modal.style.display = "none";
+  istrinti.reset();
 }
