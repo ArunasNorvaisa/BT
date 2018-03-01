22a96438f41e2bf595fec3754980289762a02642
Arunas Norvaisa <an@86.lt>
2018-03-01 12:23:12 +0200

    Registracijos projekte pradėtas rašyti JS kodas

--- js/20180227-28/scripts/script.js
+++ js/20180227-28/scripts/script.js
@@ -1,14 +1,20 @@
-$(document).ready(function(){
-    $(function() {
-        $("#inputData").datetimepicker();
-    });
-  $("#h3").click(function(){ // slepiam/rodom užduoties aprašymą
-      $("code").toggle();
+$(document).ready(function() {
+    $("#inputData").datetimepicker({ // datetimepicker pradžia
+    locale: 'lt',
+    viewMode: 'years',
+    format: 'YYYY-MM-DD',
+  }); // datetimepicker pabaiga
+  $("#h3").click(function() { // slepiam/rodom užduoties aprašymą
+    $("code").toggle();
   });
 
-    $("button[name$='mygtas']").click(function(){ // atidarom modalą
-        $('#regLangas').modal();
-        var xxx = this.name;
-        return xxx;
-    });
+  $("button[name^='mygtas']").click(function() {
+    $('#regLangas').modal(); // atidarom modalą
+    var targetId = $(this).attr('data-lentelesId'); // sekame kuris REGISTRUOTIS mygtukas paspaustas
+    $('#'+targetId).show(); // nustojame slėpti atitinkamą lentelę
+    $("button[name='mygtas']").attr({
+      data_lentelesId: targetId
+    })
+    console.log($("button[name='mygtas']").attr());
+  });
 });
