0e2bdfb8f75fea137541a1f40766aa339a776cb5
Arunas Norvaisa <an@86.lt>
2018-03-02 17:55:46 +0200

    Registracijos projekte - pirmas veikiantis variantas

--- js/20180227-28/scripts/script.js
+++ js/20180227-28/scripts/script.js
@@ -1,25 +1,35 @@
 $(document).ready(function() {
+
     $("#inputData").datetimepicker({ // datetimepicker pradžia
-    locale: 'lt',
-    viewMode: 'years',
-    format: 'YYYY-MM-DD',
-  }); // datetimepicker pabaiga
-  $("#h3").click(function() { // slepiam/rodom užduoties aprašymą
-    $("code").toggle();
-  });
+        locale: 'lt',
+        viewMode: 'decades',
+        format: 'YYYY-MM-DD',
+        defaultDate: '2000-03-05',
+        dayViewHeaderFormat: 'YYYY MMMM',
+        maxDate: '2000-03-05',
+    });
 
-  $("button[name^='mygtas']").click(function() {
-    $('#regLangas').modal(); // atidarom modalą
-    var targetId = $(this).attr('data-lentelesId'); // sekame kuris REGISTRUOTIS mygtukas paspaustas
-    $('#'+targetId).show(); // nustojame slėpti atitinkamą lentelę
-    $("button[name='mygtas']").attr("data-lentelesId", targetId); // suteikiame REGISTRUOTIS mygtukui atitinkamą atributą
-    var targetLentele = $("button[name='mygtas']").attr("data-lentelesId");
+    $("#h3").click(function() { // slepiam/rodom užduoties aprašymą
+        $("code").toggle();
+    });
 
-    $("button[name='mygtas']").click(function() {
-        console.log(targetLentele);
-        var table = $('#'+targetLentele).children();
-        table.append("<tr><td>" + $('#inputVardas').val() + "</td><td>" + $('#inputPavarde').val() + "</td><td>" + $('#inputNumeris').val() + "</td><td>" + $('#inputPastas').val() + "</td><td>" + $('#inputRenginys').val() + "</td><td>" + $('#inputSalis').val() + "</td><td>" + $('#inputKomentarai').val() + "</td></tr>");
-        console.log(table);
-    })
-  });
-});
+    $("button[name^='mygtas']").click(function() { // žalio REGISTRUOTIS mygtuko paspaudimas
+        $('#regLangas').modal(); // atidarom modalą
+        var targetId = $(this).attr('data-lentelesId'); // sekame kuris iš 4 mygtukų paspaustas
+        // suteikiame modaliniam/mėlynam REGISTRUOTIS mygtukui atitinkamą atributą:
+        $("button[name='mygtukas']").attr("data-lentelesId", targetId);
+    });
+
+    $("button[name='mygtukas']").click(function() { // f-ja pridėti eilutę prie lentelės
+        var targetId = $(this).attr('data-lentelesId'); // išsiaiškinam kurią lentelę pildysim
+        $('#' + targetId).show(); // nustojame slėpti atitinkamą lentelę
+        var table = $('#' + targetId);
+        // pridedam eilutę:
+        table.append("<tr><td>" + $('#inputVardas').val() + "</td><td>" + $('#inputPavarde').val() + "</td><td>" + $('#inputNumeris').val() + "</td><td>" + $('#inputPastas').val() + "</td><td>" + $('#inputData').val() + "</td><td>" + $('#inputRenginys').val() + "</td><td>" + $('#inputSalis').val() + "</td><td>" + $('#inputKomentarai').val() + "</td></tr>");
+        // nuimame nuo modalinio/mėlyno REGISTRUOTIS mygtuko atitinkamą atributą:
+        $("button[name='mygtukas']").removeAttr("data-lentelesId");
+        $("input", "textarea", "option").val(""); // išvalom modalo laukelius
+        $("textarea").val(""); // išvalom modalo laukelius
+        $('#regLangas').modal("hide"); // uždarom modalą
+    });
+})
