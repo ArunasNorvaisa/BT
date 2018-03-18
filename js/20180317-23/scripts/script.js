929c9e3c9789ba2a5ea2ea0bb234311e47347366
Arunas Norvaisa <an@86.lt>
2018-03-18 15:39:18 +0200

    Praktinis darbas Nr. 1 pabaigtas (???)

---
+++ js/20180317-23/scripts/script.js
@@ -0,0 +1,27 @@
+window.onload = function(){ (function(){
+
+var counter = 0,
+$items = document.querySelectorAll('.content_1 figure'),
+numItems = $items.length;
+
+var showCurrent = function(){
+var itemToShow = Math.abs(counter%numItems);
+
+[].forEach.call( $items, function(el){
+el.classList.remove('show');
+});
+
+$items[itemToShow].classList.add('show');
+};
+
+document.getElementById('next').addEventListener('click', function() {
+counter++;
+showCurrent();
+}, false);
+
+document.getElementById('prev').addEventListener('click', function() {
+counter--;
+showCurrent();
+}, false);
+
+})()};
