b93fd7bc0b483f52df9be4d1da46f9359fe9b2f1
Arunas Norvaisa <an@86.lt>
2018-03-19 10:18:22 +0200

    Praktinis darbas Nr. 1 pabaigtas (???)

--- js/20180317-23/scripts/script.js
+++ js/20180317-23/scripts/script.js
@@ -1,29 +1,31 @@
-window.onload = function(){ (function(){
+window.onload = function () {
+    (function () {
 
-var counter = 0,
-$items = document.querySelectorAll('.content_1 figure'),
-numItems = $items.length;
+        var counter = 0,
+            $items = document.querySelectorAll('.content_1 figure'),
+            numItems = $items.length;
 
-var showCurrent = function(){
-var itemToShow = Math.abs(counter % numItems);
+        var showCurrent = function () {
+            var itemToShow = Math.abs(counter % numItems);
 
-[].forEach.call( $items, function(el){
-el.classList.remove('show');
-});
+            [].forEach.call($items, function (el) {
+                el.classList.remove('show');
+            });
 
-$items[itemToShow].classList.add('show');
+            $items[itemToShow].classList.add('show');
+        };
+
+        document.getElementById('next').addEventListener('click', function () {
+            counter++;
+            showCurrent();
+        },
+            false);
+
+        document.getElementById('prev').addEventListener('click', function () {
+            counter--;
+            showCurrent();
+        },
+            false);
+
+    })()
 };
-
-document.getElementById('next').addEventListener('click', function() {
-counter++;
-showCurrent();
-},
-false);
-
-document.getElementById('prev').addEventListener('click', function() {
-counter--;
-showCurrent();
-},
-false);
-
-})()};
