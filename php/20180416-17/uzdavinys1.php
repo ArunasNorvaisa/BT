e1d9f34a04f50320e39c1a82f68fe6c20c1293e6
arunasnorvaisa <an@86.lt>
2018-04-17 09:01:22 +0300

    php 20180416-17

---
+++ php/20180416-17/uzdavinys1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Uždavinys 2</title>
+</head>
+<body>
+<h1>Sąlyga:</h1>
+<code>
+	<h2>Turime masyvą $a = ['Jonas', 'Petras', 'Antanas', 'Povilas'].
+Atspausdinkite visas galimas skirtingas poras laikant, kad pvz
+poros 'Jonas' - 'Petras' ir 'Petras' - 'Jonas' yra tokios pat.</h2>
+</code>
+<p>
+<?php
+$a = ['Jonas', 'Petras', 'Antanas', 'Povilas'];
+var_dump
+
+?>
+</p>
+</body>
+</html>
