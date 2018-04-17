560b5080b3fec767def4d0ef15f802679488c851
arunasnorvaisa <an@86.lt>
2018-04-17 09:23:39 +0300

    PHP-20180416-17 - pirmi du uždaviniai

--- php/20180416-17/uzdavinys2.php
+++ php/20180416-17/uzdavinys2.php
@@ -2,22 +2,24 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Uždavinys 1</title>
+	<title>Uždavinys 2</title>
 </head>
 <body>
 <h1>Sąlyga:</h1>
 <code>
 	<h2>Turime masyvą $a = ['Jonas', 'Petras', 'Antanas', 'Povilas'].
 Atspausdinkite visas galimas skirtingas poras laikant, kad pvz.
-poros 'Jonas' - 'Petras' ir 'Petras' - 'Jonas' yra tokios pat.</h2>
+poros 'Jonas' - 'Petras' ir 'Petras' - 'Jonas' yra skirtingos.</h2>
 </code>
 <p>
 <?php
 $a = ['Jonas', 'Petras', 'Antanas', 'Povilas'];
 
 for ($i = 0; $i < count($a); $i++) {
-    for ($j = $i + 1; $j < count($a); $j++) {
+    for ($j = 0; $j < count($a); $j++) {
+    	if ($i != $j) {
         echo '<h1>' . $a[$i] . ' - ' . $a[$j] . '</h1>';
+    }
     }
 }
 
