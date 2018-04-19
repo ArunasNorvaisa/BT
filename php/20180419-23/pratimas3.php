60d1643e27c38627b4bff6baf32b4bce523a762e
Arunas Norvaisa <an@86.lt>
2018-04-19 22:49:32 +0300

    PHP-20180419-23 klasės darbai

--- php/20180419-23/pratimas3.php
+++ php/20180419-23/pratimas3.php
@@ -4,6 +4,10 @@ class Ivykis {
 	public $pavadinimas;
 	public $data;
 
+/**
+ * @param string
+ * @param string
+ */
 	public function __construct(string $pavadinimas, string $data) {
 		$this->pavadinimas = $pavadinimas;
 		$this->data = new DateTime($data);
@@ -14,19 +18,22 @@ class Istorija {
 	public $laikotarpis;
 	public $ivykiai;
 
+/**
+ * @param string
+ * @param array
+ */
 	public function __construct(string $laikotarpis, array $ivykiai) {
 	$this->laikotarpis = $laikotarpis;
 	$this->ivykiai = $ivykiai;
-
-	public function spausdinkIstorija (array $array):string {
-		
 	}
+
+		public function spausdinkIstorija (array $array):string {			
 	}
 }
 
-$vasario16 = new Ivykis('LT nepriklausomybė', '1918-02-16');
-$zalgirioMusis = new Ivykis('Žalgirio mūšis', '1410-07-15');
-$saulesMusis = new Ivykis('Saulės mūšis', '1236-09-22');
+$vasario16 = new Ivykis ('LT nepriklausomybė', '1918-02-16');
+$zalgirioMusis = new Ivykis ('Žalgirio mūšis', '1410-07-15');
+$saulesMusis = new Ivykis ('Saulės mūšis', '1236-09-22');
 
 $ivykiai = [$vasario16, $zalgirioMusis, $saulesMusis];
 
