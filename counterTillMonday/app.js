3a9e63f06521cfb458be1f09eb10dee4191e07a9
Arunas Norvaisa <an@86.lt>
2019-03-05 07:38:33 +0200

    counter till nearest UTC Monday added

--- counterTillMonday/app.js
+++ counterTillMonday/app.js
@@ -2,19 +2,20 @@ function secondsTillMonday() {
     const currentDate = new Date(); // current date
     const mSecondsInADay = 8.64e+7; // milliseconds per day
     let days_to_monday = 8 - currentDate.getUTCDay(); // days left to closest Monday
-    if (days_to_monday === 8) {days_to_monday = 1} // solves Sunday bug
+    if (days_to_monday === 8) days_to_monday = 1; // solves Sunday bug
     const monday_in_msec = currentDate.getTime() + days_to_monday * mSecondsInADay; // earliest UTC Monday in seconds from 1970
     const next_monday = new Date(monday_in_msec); // Monday in date object
     next_monday.setUTCHours(0,0,0);
     return Math.floor((next_monday - currentDate)/1000); // Seconds till earliest UTC Monday
 }
 
-function counter(seconds) {
+function counter(seconds) {    
     document.getElementById('diff').innerHTML = seconds + 's till the nearest UTC Monday. This computes to:';
     const days = Math.floor(seconds / (24 * 3600));
     let hours = Math.floor((seconds % (24 * 3600)) / 3600);
     let minutes = Math.floor((seconds % 3600) / 60);
     seconds = seconds % 60;
+    
     if(hours < 10) hours = "0" + hours;
     if(minutes < 10) minutes = "0" + minutes;
     if(seconds < 10) seconds = "0" + seconds;
@@ -29,4 +30,5 @@ let seconds = secondsTillMonday();
 setInterval(function() {
     counter(seconds);
     seconds--;
+    if(seconds === 0) seconds = 7 * 24 * 3600;
 }, 1000);
