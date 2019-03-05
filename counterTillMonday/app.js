d4b50a1aa62ab9badbcd56d74403e3743ee00a13
Arunas Norvaisa <an@86.lt>
2019-03-05 07:15:48 +0200

    counter till nearest UTC Monday added

--- counterTillMonday/app.js
+++ counterTillMonday/app.js
@@ -15,9 +15,9 @@ function counter(seconds) {
     let hours = Math.floor((seconds % (24 * 3600)) / 3600);
     let minutes = Math.floor((seconds % 3600) / 60);
     seconds = seconds % 60;
-	if(hours < 10) hours = "0" + hours;
-	if(minutes < 10) minutes = "0" + minutes;
-	if(seconds < 10) seconds = "0" + seconds;
+    if(hours < 10) hours = "0" + hours;
+    if(minutes < 10) minutes = "0" + minutes;
+    if(seconds < 10) seconds = "0" + seconds;
 
     document.getElementById('days').innerHTML = days + ' day(s)';
     document.getElementById('hours').innerHTML= hours + ' hour(s)';
